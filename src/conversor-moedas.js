aebf65468da12ea38603a4e817ff8f5f5db0353a
fas-fernando <fas.alves.souza@gmail.com>
2020-06-06 20:47:50 -0300

    inserido as funcionalidades necessárias para a conversão

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './conversor-moedas.css';
 import { 
   Jumbotron, Form, Container, Col, Button, Spinner, Alert, Modal
@@ -6,8 +6,65 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 import ListarMoedas from './listar-moedas';
+import axios from 'axios';
 
 function ConversorMoedas() {
+
+  const FIXER_URL = 'http://data.fixer.io/api/latest?access_key=a246beab6c15d6b142e1b3404d0ff3f6';
+
+  const [ valor, setValor ] = useState('1');
+  const [ moedaDe, setMoedaDe ] = useState('BRL');
+  const [ moedaPara, setMoedaPara ] = useState('USD');
+  const [ exibirSpinner, setExibirSpinner ] = useState(false);
+  const [ formValidado, setFormValidado ] = useState(false);
+  const [ exibirModal, setExibirModal ] = useState(false);
+  const [ resultadoConversao, setResultadoConversao ] = useState('');
+
+  function handleValor (event) {
+    setValor(event.target.value.replace(/\D/g, ''));
+  }
+
+  function handleMoedaDe (event) {
+    setMoedaDe(event.target.value);
+  }
+
+  function handleMoedaPara (event) {
+    setMoedaPara(event.target.value);
+  }
+
+  function handleFecharModal (event) {
+    setValor('1');
+    setMoedaDe('BRL');
+    setMoedaPara('USD');
+    setFormValidado(false);
+    setExibirModal(false);
+  }
+
+  function converter (event) {
+    event.preventDefault();
+    setFormValidado(true);
+    if (event.currentTarget.checkValidity() === true){
+      setExibirSpinner(true);
+      axios.get(FIXER_URL)
+        .then(res => {
+          const cotacao = obterCotacao(res.data);
+          setResultadoConversao(`${valor} ${moedaDe} = ${cotacao} ${moedaPara}`);
+          setExibirModal(true);
+          setExibirSpinner(false);
+        });
+    }
+  }
+
+  function obterCotacao (dadosCotacao) {
+    if(!dadosCotacao || dadosCotacao.success !== true) {
+      return false;
+    }
+    const cotacaoDe = dadosCotacao.rates[moedaDe];
+    const cotacaoPara = dadosCotacao.rates[moedaPara];
+    const cotacao = (1 / cotacaoDe * cotacaoPara) * valor;
+    return cotacao.toFixed(2);
+  }
+
   return (
     <div>
       <h1>Conversor de Moedas</h1>
@@ -16,17 +73,19 @@ function ConversorMoedas() {
       </Alert>
       <Jumbotron fluid>
         <Container>
-          <Form>
+          <Form onSubmit={converter} noValidate validated={formValidado}>
             <Form.Row>
               <Col sm="3">
                 <Form.Control 
                   placeholder="0"
-                  value={1}
-                  riquired
-                />
+                  value={valor}
+                  onChange={handleValor}
+                  riquired/>
               </Col>
               <Col sm="3">
-                <Form.Control as="select">
+                <Form.Control as="select"
+                  value={moedaDe}
+                  onChange={handleMoedaDe}>
                   <ListarMoedas />
                 </Form.Control>
               </Col>
@@ -36,27 +95,37 @@ function ConversorMoedas() {
                 <FontAwesomeIcon icon={faAngleDoubleRight} />
               </Col>
               <Col sm="3">
-                <Form.Control as="select">
+                <Form.Control as="select"
+                  value={moedaPara}
+                  onChange={handleMoedaPara}>
                   <ListarMoedas />
                 </Form.Control>
               </Col>
               <Col sm="2">
-                <Button variant="success" type="submit">
-                  <Spinner animation="border" size="sm" />
-                  Converter
+                <Button 
+                  variant="success" 
+                  type="submit">
+                  <span className={exibirSpinner ? null : 'hidden'}>
+                    <Spinner animation="border" size="sm" />
+                  </span>
+                  <span className={exibirSpinner ? 'hidden' : null}>
+                    Converter
+                  </span>
                 </Button>
               </Col>
             </Form.Row>
           </Form>
-          <Modal show={false}>
+          <Modal show={exibirModal} onHide={handleFecharModal}>
             <Modal.Header closeButton>
               <Modal.Title>Reasultado da Conversão</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              <p>Modal body text goes here.</p>
+              {resultadoConversao}
             </Modal.Body>
             <Modal.Footer>
-              <Button variant="success">Nova Conversão</Button>
+              <Button variant="success" onClick={handleFecharModal}>
+                Nova Conversão
+              </Button>
             </Modal.Footer>
           </Modal>
         </Container>
